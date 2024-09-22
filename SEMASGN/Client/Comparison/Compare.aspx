884d2b69d210ace8599858428b31909375e392c5
Dengwait <tdwei716@gmail.com>
2024-09-22 09:39:13 +0800

    compare programme

---
+++ SEMASGN/Client/Comparison/Compare.aspx
@@ -0,0 +1,217 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Client/MasterPage/MasterPage.Master" AutoEventWireup="true" CodeBehind="Compare.aspx.cs" Inherits="SEMASGN.Client.Comparison.WebForm1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="w-[80%] min-h-80 border border-black m-auto my-10 text-justify">
+        <!-- Programme -->
+        <div class="w-[100%] min-h-10 border-b border-black flex">
+            <div class="w-[20%] min-h-full flex justify-center  p-2">
+                <p>Programme</p>
+            </div>
+            <div class="w-[80%] min-h-full flex">
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:DropDownList ID="ddlProgramme1" runat="server" AutoPostBack="True" CssClass="form-control" OnSelectedIndexChanged="ddlProgramme1_SelectedIndexChanged"></asp:DropDownList>
+                </div>
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:DropDownList ID="ddlProgramme2" runat="server" AutoPostBack="True" CssClass="form-control" OnSelectedIndexChanged="ddlProgramme2_SelectedIndexChanged"></asp:DropDownList>
+                </div>
+            </div>
+        </div>
+        <!-- Row 1: Programme ID -->
+        <div class="w-[100%] min-h-10 border-b border-black flex">
+            <div class="w-[20%] min-h-full flex justify-center p-2"">
+                <p>Programme ID</p>
+            </div>
+            <div class="w-[80%] min-h-full flex">
+                <div class="w-[50%]  border-l border-black break-words p-2"">
+                    <asp:Label ID="lblProgramme1ID" runat="server"></asp:Label>
+                </div>
+                <div class="w-[50%]  border-l border-black break-words p-2"">
+                    <asp:Label ID="lblProgramme2ID" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+
+        <!-- Row 2: Description -->
+        <div class="w-[100%] min-h-10 border-b border-black flex">
+            <div class="w-[20%] min-h-full  flex justify-center p-2">
+                <p>Description</p>
+            </div>
+            <div class="w-[80%] min-h-full flex">
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme1Description" runat="server"></asp:Label>
+                </div>
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme2Description" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+
+        <!-- Row 3: Local Student Fees -->
+        <div class="w-[100%] min-h-10 border-b border-black flex">
+            <div class="w-[20%] min-h-full flex justify-center p-2">
+                <p>Local Student Fees</p>
+            </div>
+            <div class="w-[80%] min-h-full flex">
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme1LocalFees" runat="server"></asp:Label>
+                </div>
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme2LocalFees" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+
+        <!-- Row 4: International Student Fees -->
+        <div class="w-[100%] min-h-10 border-b border-black flex">
+            <div class="w-[20%] min-h-full flex justify-center p-2">
+                <p>International Student Fees</p>
+            </div>
+            <div class="w-[80%] min-h-full flex">
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme1InternationalFees" runat="server"></asp:Label>
+                </div>
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme2InternationalFees" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+
+        <!-- Row 5: Programme Outline -->
+        <div class="w-[100%] min-h-10 border-b border-black flex">
+            <div class="w-[20%] min-h-full flex justify-center p-2">
+                <p>Programme Outline</p>
+            </div>
+            <div class="w-[80%] min-h-full  flex">
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme1MainCourse" runat="server"></asp:Label>
+                </div>
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme2MainCourse" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+
+        <!-- Row 6: Minimum Requirements - Local -->
+        <div class="w-[100%] min-h-10 border-b border-black flex">
+            <div class="w-[20%] min-h-full flex justify-center p-2">
+                <p>Minimum Requirements (Local)</p>
+            </div>
+            <div class="w-[80%] min-h-full flex">
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme1LocalMinReq" runat="server"></asp:Label>
+                </div>
+                <div class="w-[50%] border-l border-black break-word p-2">
+                    <asp:Label ID="lblProgramme2LocalMinReq" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+
+        <!-- Row 7: Minimum Requirements - International -->
+        <div class="w-[100%] min-h-10 border-b border-black flex">
+            <div class="w-[20%] min-h-full  flex justify-center p-2">
+                <p>Minimum Requirements (International)</p>
+            </div>
+            <div class="w-[80%] min-h-full  flex">
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme1InternationalMinReq" runat="server"></asp:Label>
+                </div>
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme2InternationalMinReq" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+
+        <!-- Row 8: Career Prospects -->
+        <div class="w-[100%] min-h-10 border-b border-black flex">
+            <div class="w-[20%] min-h-full flex justify-center p-2">
+                <p>Career Prospects</p>
+            </div>
+            <div class="w-[80%] min-h-full flex">
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme1CareerProspects" runat="server"></asp:Label>
+                </div>
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme2CareerProspects" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Row 10: Full-Time Duration -->
+        <div class="w-[100%] min-h-10 border-b border-black flex">
+            <div class="w-[20%] min-h-full flex justify-center p-2">
+                <p>Full-Time Duration</p>
+            </div>
+            <div class="w-[80%] min-h-full  flex">
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme1FTDuration" runat="server"></asp:Label>
+                </div>
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme2FTDuration" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Row 9: Part-Time Duration -->
+        <div class="w-[100%] min-h-10 border-b border-black flex">
+            <div class="w-[20%] min-h-full  flex justify-center p-2">
+                <p>Part-Time Duration</p>
+            </div>
+            <div class="w-[80%] min-h-full flex">
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme1PTDuration" runat="server"></asp:Label>
+                </div>
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme2PTDuration" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+        <!-- Row 11: Type -->
+        <div class="w-[100%] min-h-10 border-b border-black flex">
+            <div class="w-[20%] min-h-full flex justify-center p-2">
+                <p>Type</p>
+            </div>
+            <div class="w-[80%] min-h-full flex">
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme1Type" runat="server"></asp:Label>
+                </div>
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme2Type" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+
+        <!-- Row 12: Campus-->
+        <div class="w-[100%] min-h-10 border-b border-black flex">
+            <div class="w-[20%] min-h-full flex justify-center p-2">
+                <p>Campus</p>
+            </div>
+            <div class="w-[80%] min-h-full flex">
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme1Campus" runat="server"></asp:Label>
+                </div>
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme2Campus" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+
+        <!-- Row 13: Intake-->
+        <div class="w-[100%] min-h-10 flex">
+            <div class="w-[20%] min-h-full flex justify-center p-2">
+                <p>Intake</p>
+            </div>
+            <div class="w-[80%] min-h-full flex">
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme1Intake" runat="server"></asp:Label>
+                </div>
+                <div class="w-[50%] border-l border-black break-words p-2">
+                    <asp:Label ID="lblProgramme2Intake" runat="server"></asp:Label>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</asp:Content>
---
+++ SEMASGN/Client/Comparison/Compare.aspx.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SEMASGN.Client.Comparison
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Load the dropdown lists with programme data on first load.
+                LoadProgrammes();
+            }
+
+        }
+
+
+        protected void ddlProgramme1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string programmeId = ddlProgramme1.SelectedValue;
+            
+            if(ddlProgramme1.SelectedIndex == 0)
+            {
+                lblProgramme1ID.Text = "";
+                lblProgramme1Description.Text = "";
+                lblProgramme1LocalFees.Text = ""; 
+                lblProgramme1InternationalFees.Text = "";
+                lblProgramme1MainCourse.Text = "";
+                lblProgramme1LocalMinReq.Text = "";
+                lblProgramme1InternationalMinReq.Text = ""; 
+                lblProgramme1CareerProspects.Text = "";
+                lblProgramme1PTDuration.Text = ""; 
+                lblProgramme1FTDuration.Text = "";
+                lblProgramme1Type.Text = "";
+                lblProgramme1Campus.Text = ""; 
+                lblProgramme1Intake.Text = "";
+            }
+            RetrieveProgrammeDetails(programmeId, lblProgramme1ID, lblProgramme1Description, lblProgramme1LocalFees, lblProgramme1InternationalFees,
+                                      lblProgramme1MainCourse, lblProgramme1LocalMinReq, lblProgramme1InternationalMinReq, lblProgramme1CareerProspects,
+                                      lblProgramme1PTDuration, lblProgramme1FTDuration, lblProgramme1Type, lblProgramme1Campus, lblProgramme1Intake);
+        }
+
+        protected void ddlProgramme2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string programmeId = ddlProgramme2.SelectedValue;
+            if (ddlProgramme2.SelectedIndex == 0)
+            {
+                lblProgramme2ID.Text = "";
+                lblProgramme2Description.Text = "";
+                lblProgramme2LocalFees.Text = "";
+                lblProgramme2InternationalFees.Text = "";
+                lblProgramme2MainCourse.Text = "";
+                lblProgramme2LocalMinReq.Text = "";
+                lblProgramme2InternationalMinReq.Text = "";
+                lblProgramme2CareerProspects.Text = "";
+                lblProgramme2PTDuration.Text = "";
+                lblProgramme2FTDuration.Text = "";
+                lblProgramme2Type.Text = "";
+                lblProgramme2Campus.Text = "";
+                lblProgramme2Intake.Text = "";
+            }
+            RetrieveProgrammeDetails(programmeId, lblProgramme2ID, lblProgramme2Description, lblProgramme2LocalFees, lblProgramme2InternationalFees,
+                                      lblProgramme2MainCourse, lblProgramme2LocalMinReq, lblProgramme2InternationalMinReq, lblProgramme2CareerProspects,
+                                      lblProgramme2PTDuration, lblProgramme2FTDuration, lblProgramme2Type, lblProgramme2Campus, lblProgramme2Intake);
+        }
+
+        private void RetrieveProgrammeDetails(string programmeId, Label lblID, Label lblDescription, Label lblLocalFees, Label lblInternationalFees,
+                                               Label lblMainCourse, Label lblLocalMinReq, Label lblInternationalMinReq, Label lblCareerProspects,
+                                               Label lblPTDuration, Label lblFTDuration, Label lblType, Label lblCampus, Label lblIntake)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            string query = "SELECT * FROM Programme WHERE id = @progId;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@progId", programmeId);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                reader.Read();
+
+                                lblID.Text = string.IsNullOrEmpty(reader["id"].ToString()) ? "-" : reader["id"].ToString();
+                                lblDescription.Text = string.IsNullOrEmpty(reader["description"].ToString()) ? "-" : reader["description"].ToString();
+                                lblLocalFees.Text = string.IsNullOrEmpty(reader["localFees"].ToString()) ? "-" : reader["localFees"].ToString();
+                                lblInternationalFees.Text = string.IsNullOrEmpty(reader["internationalFees"].ToString()) ? "-" : reader["internationalFees"].ToString();
+
+                                // Deserialize JSON for local minimum requirements and append localSpecificSubjectReq (if exists)
+                                string localMinReqJson = reader["localMinReq"].ToString();
+                                string localSpecificSubjectReq = reader["localSpecificSubjectReq"].ToString();
+                                if (!string.IsNullOrEmpty(localMinReqJson))
+                                {
+                                    var localMinReq = new JavaScriptSerializer().Deserialize<Dictionary<string, string>>(localMinReqJson);
+                                    lblLocalMinReq.Text = FormatRequirements(localMinReq, localSpecificSubjectReq);
+                                }
+                                else
+                                {
+                                    lblLocalMinReq.Text = "-";
+                                }
+
+                                // Deserialize JSON for international minimum requirements and append internationalSpecificSubjectReq (if exists)
+                                string internationalMinReqJson = reader["internationalMinReq"].ToString();
+                                string internationalSpecificSubjectReq = reader["internationalSpecificSubjectReq"].ToString();
+                                if (!string.IsNullOrEmpty(internationalMinReqJson))
+                                {
+                                    var internationalMinReq = new JavaScriptSerializer().Deserialize<Dictionary<string, string>>(internationalMinReqJson);
+                                    lblInternationalMinReq.Text = FormatRequirements(internationalMinReq, internationalSpecificSubjectReq);
+                                }
+                                else
+                                {
+                                    lblInternationalMinReq.Text = "-";
+                                }
+
+                                string mainCourse = reader["mainCourse"].ToString();
+                                string mpuCourses = reader["mpu"].ToString();
+                                lblMainCourse.Text = FormatCourses(mainCourse, mpuCourses); lblCareerProspects.Text = string.IsNullOrEmpty(reader["careerProspects"].ToString()) ? "-" : FormatCareerProspects(reader["careerProspects"].ToString());
+                                lblPTDuration.Text = string.IsNullOrEmpty(reader["ptDuration"].ToString()) ? "-" : reader["ptDuration"].ToString();
+                                lblFTDuration.Text = string.IsNullOrEmpty(reader["ftDuration"].ToString()) ? "-" : reader["ftDuration"].ToString();
+                                lblType.Text = string.IsNullOrEmpty(reader["type"].ToString()) ? "-" : reader["type"].ToString();
+                                lblCampus.Text = string.IsNullOrEmpty(reader["campus"].ToString()) ? "-" : reader["campus"].ToString();
+                                lblIntake.Text = string.IsNullOrEmpty(reader["intake"].ToString()) ? "-" : reader["intake"].ToString();
+                            }
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    // Handle exceptions (e.g., log them)
+                }
+            }
+        }
+
+        private string FormatCourses(string mainCourse, string mpuCourses)
+        {
+            string formattedMainCourse = string.IsNullOrEmpty(mainCourse) ? "-" : string.Join("<br/>", mainCourse.Split(',').Select(c => c.Trim()));
+
+            if (!string.IsNullOrEmpty(mpuCourses))
+            {
+                formattedMainCourse += "<br/><br/>MPU Courses:<br/>" + string.Join("<br/>", mpuCourses.Split(',').Select(c => c.Trim()));
+            }
+
+            return formattedMainCourse;
+        }
+
+        // Helper function to format JSON data and append subject-specific requirements
+        private string FormatRequirements(Dictionary<string, string> requirements, string specificSubjectReq)
+        {
+            string formatted = string.Join("<br/>", requirements.Select(kv => $"{kv.Key}: {kv.Value}"));
+
+            // Append subject-specific requirements if available
+            if (!string.IsNullOrEmpty(specificSubjectReq))
+            {
+                var specificReqs = new JavaScriptSerializer().Deserialize<Dictionary<string, string>>(specificSubjectReq);
+                formatted += "<br/><br/>Specific Subject Requirement:<br/>" + string.Join("<br/>", specificReqs.Select(kv => $"{kv.Key}: {kv.Value}"));
+            }
+
+            return formatted;
+        }
+
+        private void LoadProgrammes()
+        {
+            // Get connection string from Web.config
+            string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT id, name FROM Programme;";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    // Bind Programme data to DropDownList 1
+                    ddlProgramme1.DataSource = reader;
+                    ddlProgramme1.DataTextField = "name";
+                    ddlProgramme1.DataValueField = "id";
+                    ddlProgramme1.DataBind();
+                    ddlProgramme1.Items.Insert(0, new ListItem("Select a programme", null));
+
+                    reader.Close();
+
+                    // Execute reader again for the other dropdowns
+                    reader = cmd.ExecuteReader();
+
+                    // Bind Programme data to DropDownList 2
+                    ddlProgramme2.DataSource = reader;
+                    ddlProgramme2.DataTextField = "name";
+                    ddlProgramme2.DataValueField = "id";
+                    ddlProgramme2.DataBind();
+                    ddlProgramme2.Items.Insert(0, new ListItem("Select a programme", null));
+
+                    reader.Close();
+
+                }
+            }
+        }
+
+        // New helper function to format career prospects
+        private string FormatCareerProspects(string careerProspects)
+        {
+            return string.Join("<br/>", careerProspects.Split(',').Select(c => c.Trim()));
+        }
+    }
+}
